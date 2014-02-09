c5c5fa606c68c56acec50891e14f626d19429a66
denyspro <denysbrag@gmail.com>
2014-02-09 16:44:46 -0200

    Prova de conceito da listagem de usuários: - Remoção da camada de serviços, deixando somente as camadas de mapeamento e de negócios para simplificar a solução. - Configuração do filtro do ActiveJdbc para que a conexão seja completamente controlada pelo container (pool no JBoss e abertura e fechamento de conexões pelo filtro).

--- edipo/src/main/java/br/com/edipo/ada/domain/UsuarioBean.java
+++ edipo/src/main/java/br/com/edipo/ada/domain/UsuarioBean.java
@@ -1,25 +1,21 @@
 package br.com.edipo.ada.domain;
 
-import br.com.edipo.ada.model.entity.Usuario;
-import br.com.edipo.ada.model.persistence.UsuarioDAO;
-
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
-import javax.inject.Inject;
-
 import java.util.List;
 
-@ManagedBean
+import javax.faces.bean.ViewScoped;
+import javax.faces.bean.ManagedBean;
+
+import br.com.edipo.ada.model.entity.Usuario;
+
 @ViewScoped
+@ManagedBean
 public class UsuarioBean {
-	@Inject
-	private UsuarioDAO dao;
 
 	private List<Usuario> usuarios;
 
-	public List<Usuario> listaUsuario() {
+	public List<Usuario> getUsuarios() {
 		if (usuarios == null) {
-			usuarios = dao.lista();
+			usuarios = Usuario.findAll();
 		}
 		return usuarios;
 	}
